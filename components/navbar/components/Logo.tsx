b98798ce337bb6c29038412db876ab80df787ea3
E. Hannah <echannah631@gmail.com>
2024-12-30 13:11:20 -0500

    working on logo button in navbar

--- components/navbar/components/Logo.tsx
+++ components/navbar/components/Logo.tsx
@@ -1,7 +1,26 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import React from 'react';
+import logo from '../../../public/logo/logo-color-2.svg';
+import Image from 'next/image';
 
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <Button
+      size='lg'
+      asChild
+      variant='link'
+    >
+      <Link href='/'>
+        <Image
+          src={logo}
+          alt='logo'
+          width={64}
+          height={64}
+        />
+      </Link>
+    </Button>
+  );
 };
 
 export default Logo;
