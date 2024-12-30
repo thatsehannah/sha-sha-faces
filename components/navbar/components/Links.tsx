28fbec9d02061bbf22570692aac2320498bdfe4d
E. Hannah <echannah631@gmail.com>
2024-12-30 13:23:22 -0500

    refactoring links component

--- components/navbar/components/Links.tsx
+++ components/navbar/components/Links.tsx
@@ -3,42 +3,29 @@ import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import React from 'react';
 
+const LinkButton = ({ page }: { page: string }) => {
+  return (
+    <Button
+      variant='ghost'
+      asChild
+    >
+      <Link
+        href={`/${page}`}
+        className='text-lg capitalize'
+      >
+        {page}
+      </Link>
+    </Button>
+  );
+};
+
 const Links = () => {
   return (
     <Container className='flex justify-evenly gap-4'>
-      <Button
-        variant='ghost'
-        asChild
-      >
-        <Link
-          href='/about'
-          className='text-lg'
-        >
-          About
-        </Link>
-      </Button>
-      <Button
-        variant='ghost'
-        asChild
-      >
-        <Link
-          href='/portfolio'
-          className='text-lg'
-        >
-          Portfolio
-        </Link>
-      </Button>
-      <Button
-        variant='ghost'
-        asChild
-      >
-        <Link
-          href='/contact'
-          className='text-lg'
-        >
-          Contact
-        </Link>
-      </Button>
+      <LinkButton page='about' />
+      <LinkButton page='portfolio' />
+      <LinkButton page='contact' />
+      <LinkButton page='services' />
     </Container>
   );
 };
