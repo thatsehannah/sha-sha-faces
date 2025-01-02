4592593daca261ed39a8c8e085b86c4a8a830d82
E. Hannah <echannah631@gmail.com>
2025-01-02 13:46:39 -0500

    replacing hero carousel with a static image (will use carousel in portfolio gallery)

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -3,20 +3,29 @@ import { Button } from '../ui/button';
 import Link from 'next/link';
 import Image from 'next/image';
 import { asap } from '@/lib/fonts';
-import HeroCarousel from './components/HeroCarousel';
+import heroImg from '@/public/images/photo6.jpeg';
 
 const Hero = () => {
   return (
-    <section className='mx-auto grid grid-cols-1 lg:grid-cols-2 gap-24 px-24 py-8 items-center bg-gradient-to-tl from-soft-pink to-white'>
+    <section className='mx-auto grid grid-cols-1 lg:grid-cols-2 gap-24 px-10 lg:px-24 py-8 items-center bg-gradient-to-tl from-soft-pink to-white'>
       <div className='flex flex-col justify-center items-center'>
+        <Image
+          src={heroImg}
+          height={500}
+          width={500}
+          alt='hero image'
+          priority
+          className='block lg:hidden w-full h-[25rem] mb-8 rounded-md object-cover'
+        />
         <Image
           src='/logo/logo-color-2.svg'
           alt='logo'
           width={400}
           height={400}
+          className='h-[16rem] lg:h-[24rem]'
         />
         <p
-          className={`${asap.className} text-2xl font-extrabold uppercase mt-4`}
+          className={`${asap.className} text-lg lg:text-2xl font-extrabold uppercase mt-4`}
         >
           Glam that soothes, beauty that shines
         </p>
@@ -28,7 +37,14 @@ const Hero = () => {
           <Link href='/book'>View Portfolio &gt;</Link>
         </Button>
       </div>
-      <HeroCarousel />
+      <Image
+        src={heroImg}
+        height={500}
+        width={500}
+        alt='hero image'
+        priority
+        className='hidden lg:block w-full h-[44rem] rounded-md object-cover'
+      />
     </section>
   );
 };
