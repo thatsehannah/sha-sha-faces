d5bb99e7c6d6ad26886e958d02835f111488f298
E. Hannah <echannah631@gmail.com>
2025-01-03 00:09:46 -0500

    creating bridal and glam gallery pages

---
+++ app/gallery/bridal/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BridalGallery = () => {
+  return <div>Bridal Gallery</div>;
+};
+
+export default BridalGallery;
---
+++ app/gallery/glam/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GlamGallery = () => {
+  return <div>Glam Gallery</div>;
+};
+
+export default GlamGallery;
