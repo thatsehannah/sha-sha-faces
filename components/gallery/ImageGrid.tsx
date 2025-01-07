d9e86b9e15e109095bd4b4132ba321f6cb323bb0
E. Hannah <echannah631@gmail.com>
2025-01-07 09:58:33 -0500

    staggering images in image grid component with framer

--- components/gallery/ImageGrid.tsx
+++ components/gallery/ImageGrid.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Image from 'next/image';
 import React from 'react';
+import { motion } from 'framer-motion';
 import Container from '../global/Container';
 
 type ImageGridProps = {
@@ -10,21 +13,45 @@ type ImageGridProps = {
 };
 
 const ImageGrid = ({ photos }: ImageGridProps) => {
+  const gridVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { staggerChildren: 0.1, delayChildren: 0.2 },
+    },
+  };
+
+  const imageVariant = {
+    hidden: { opacity: 0, y: -50 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
     <Container className='columns-2 lg:columns-5'>
-      {photos.map((img, idx) => {
-        return (
-          <Image
-            src={img.url}
-            alt={img.alt}
-            key={idx}
-            width={500}
-            height={500}
-            sizes='100vw'
-            className='rounded-md object-cover mb-4'
-          />
-        );
-      })}
+      <motion.div
+        variants={gridVariants}
+        initial='hidden'
+        animate='visible'
+      >
+        {photos.map((img, idx) => {
+          return (
+            <motion.div
+              key={idx}
+              variants={imageVariant}
+            >
+              <Image
+                src={img.url}
+                alt={img.alt}
+                key={idx}
+                width={500}
+                height={500}
+                sizes='100vw'
+                className='rounded-md object-cover mb-4'
+              />
+            </motion.div>
+          );
+        })}
+      </motion.div>
     </Container>
   );
 };
