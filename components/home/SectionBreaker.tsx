317304d1452f79c3db028139e9c204604129d9b3
E. Hannah <echannah631@gmail.com>
2025-01-10 18:45:28 -0500

    hiding section breaker on mobile

--- components/home/SectionBreaker.tsx
+++ components/home/SectionBreaker.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SectionBreaker = () => {
   return (
-    <div className='w-screen h-[35rem] bg-center bg-cover bg-fixed bg-no-repeat bg-section-breaker' />
+    <div className='hidden lg:block w-screen h-[35rem] bg-center bg-cover bg-fixed bg-no-repeat bg-section-breaker' />
   );
 };
 
