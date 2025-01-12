5819b3c86b6384f4fc77f72fbea5bac432e9f2e4
E. Hannah <echannah631@gmail.com>
2025-01-12 14:17:48 -0500

    fixing section breaker for mobile browsers again (didn't fixing the first time)

--- components/home/SectionBreaker.tsx
+++ components/home/SectionBreaker.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SectionBreaker = () => {
   return (
     <div className='w-full h-[28rem] xl:h-[35rem]'>
-      <div className='w-full h-full bg-center bg-cover bg-fixed bg-no-repeat bg-section-breaker' />
+      <div className='w-full h-full bg-center bg-cover bg-local xl:bg-fixed bg-no-repeat bg-section-breaker' />
     </div>
   );
 };
