3029c2b0ba5666d4c9b7dfb1a17e19411430f5fb
E. Hannah <echannah631@gmail.com>
2025-01-12 09:45:06 -0500

    fixing section breaker component to be fixed across all screen sizes

--- components/home/SectionBreaker.tsx
+++ components/home/SectionBreaker.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const SectionBreaker = () => {
   return (
-    <div className='w-full h-[35rem] bg-center bg-cover xl:bg-fixed bg-scroll bg-no-repeat bg-section-breaker' />
+    <div className='w-full h-[28rem] xl:h-[35rem]'>
+      <div className='w-full h-full bg-center bg-cover bg-fixed bg-no-repeat bg-section-breaker' />
+    </div>
   );
 };
 
