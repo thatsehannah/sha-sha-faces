a9d0af8c1663874ce2b3c11ed0cb8b1ed0a9ffa9
E. Hannah <echannah631@gmail.com>
2025-01-22 15:18:47 -0600

    creating sidebar page for admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,22 +1,25 @@
+import AdminSidebar from '@/components/dashboard/AdminSidebar';
 import Container from '@/components/global/Container';
 import SectionTitle from '@/components/global/SectionTitle';
+import { SidebarProvider } from '@/components/ui/sidebar';
 import React, { ReactNode } from 'react';
 
-const AdminLayout = ({ children }: { children: ReactNode }) => {
+export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
-    <div className='relative'>
-      <SectionTitle
-        title='dashboard'
-        alignment='right'
-      />
-      <section className='py-20'>
-        <Container className='grid lg: grid-cols-12 gap-12'>
-          <div className='lg:col-span-2'>Sidebar</div>
-          <div className='lg:col-span-10'>{children}</div>
-        </Container>
-      </section>
-    </div>
-  );
-};
+    <SidebarProvider>
+      <AdminSidebar />
 
-export default AdminLayout;
+      <main className='relative'>
+        <SectionTitle
+          title='dashboard'
+          alignment='right'
+        />
+        <section className='py-20'>
+          <Container className='grid lg: grid-cols-12 gap-12'>
+            <div className='lg:col-span-10'>{children}</div>
+          </Container>
+        </section>
+      </main>
+    </SidebarProvider>
+  );
+}
---
+++ components/dashboard/AdminSidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Sidebar, SidebarContent } from '../ui/sidebar';
+
+const AdminSidebar = () => {
+  return (
+    <Sidebar>
+      <SidebarContent />
+    </Sidebar>
+  );
+};
+
+export default AdminSidebar;
