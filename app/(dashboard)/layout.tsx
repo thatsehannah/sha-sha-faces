2eb61bb4fc20637d3d65865ea615f86bfb7c8ab2
E. Hannah <echannah631@gmail.com>
2025-01-23 18:12:58 -0500

    adding theme and toast providers to dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,6 +3,7 @@ import Container from '@/components/global/Container';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import React, { ReactNode } from 'react';
 import '../globals.css';
+import Providers from '../providers';
 import { afacadFlux } from '@/lib/fonts';
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
@@ -12,16 +13,18 @@ export default function AdminLayout({ children }: { children: ReactNode }) {
       suppressHydrationWarning
     >
       <body className={`${afacadFlux.className}`}>
-        <SidebarProvider>
-          <AdminSidebar />
-          <main className='relative'>
-            <section className='py-20'>
-              <Container className='grid lg: grid-cols-12 gap-12'>
-                <div className='lg:col-span-10'>{children}</div>
-              </Container>
-            </section>
-          </main>
-        </SidebarProvider>
+        <Providers>
+          <SidebarProvider>
+            <AdminSidebar />
+            <main className='relative'>
+              <section className='py-20'>
+                <Container className='grid lg: grid-cols-12 gap-12'>
+                  <div className='lg:col-span-10'>{children}</div>
+                </Container>
+              </section>
+            </main>
+          </SidebarProvider>
+        </Providers>
       </body>
     </html>
   );
