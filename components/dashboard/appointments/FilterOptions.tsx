0787bfddd81d98473f2105a1d663ae83708ceac7
E. Hannah <echannah631@gmail.com>
2025-01-27 03:29:48 -0500

    creating a mobile dropdown menu for filter appointments

--- components/dashboard/appointments/FilterOptions.tsx
+++ components/dashboard/appointments/FilterOptions.tsx
@@ -2,6 +2,15 @@
 
 import { Label } from '@/components/ui/label';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { STATUSES } from '@/utils/constants';
 import { useRouter, useSearchParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
@@ -32,32 +41,66 @@ const FilterOptions = () => {
   }, [filterParam]);
 
   return (
-    <RadioGroup
-      value={filterValue}
-      orientation='horizontal'
-      className='flex gap-8'
-      onValueChange={handleFilter}
-    >
-      <div className='flex items-center space-x-2'>
-        <RadioGroupItem
-          value='all'
-          id='all'
-        />
-        <Label htmlFor='all'>All</Label>
-      </div>
-      {STATUSES.map((status, idx) => (
-        <div
-          key={idx}
-          className='flex items-center space-x-2'
+    <>
+      <div className='hidden lg:block'>
+        <RadioGroup
+          value={filterValue}
+          orientation='horizontal'
+          className='flex gap-8'
+          onValueChange={handleFilter}
         >
-          <RadioGroupItem
-            value={status.toLowerCase()}
-            id={status}
-          />
-          <Label htmlFor={status}>{status}</Label>
-        </div>
-      ))}
-    </RadioGroup>
+          <div className='flex items-center space-x-2'>
+            <RadioGroupItem
+              value='all'
+              id='all'
+            />
+            <Label htmlFor='all'>All</Label>
+          </div>
+          {STATUSES.map((status, idx) => (
+            <div
+              key={idx}
+              className='flex items-center space-x-2'
+            >
+              <RadioGroupItem
+                value={status.toLowerCase()}
+                id={status}
+              />
+              <Label htmlFor={status}>{status}</Label>
+            </div>
+          ))}
+        </RadioGroup>
+      </div>
+      <div className='lg:hidden flex'>
+        <Select
+          value={filterValue}
+          onValueChange={handleFilter}
+        >
+          <SelectTrigger className='text-lg'>
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Filter</SelectLabel>
+              <SelectItem
+                value='all'
+                className='text-lg'
+              >
+                All
+              </SelectItem>
+              {STATUSES.map((status, idx) => (
+                <SelectItem
+                  key={idx}
+                  value={status.toLowerCase()}
+                  className='text-lg'
+                >
+                  {status}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+    </>
   );
 };
 
