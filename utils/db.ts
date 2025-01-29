7d2c17df92ed13e6e1443aecd3a5c3fb4cd4978d
E. Hannah <echannah631@gmail.com>
2025-01-29 12:48:16 -0500

    reverting prisma client back as this wasn't the issue

--- utils/db.ts
+++ utils/db.ts
@@ -10,17 +10,8 @@ declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-  // In production, reuse the Prisma client instance for performance reasons
-  prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-  if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
-} else {
-  // In development mode, always create a new Prisma client instance
-  prisma = prismaClientSingleton();
-}
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
 
-// if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
