416dbf2bb7bbb8ef36ce18b4e9fe59afefa281c6
E. Hannah <echannah631@gmail.com>
2025-01-29 12:40:16 -0500

    seeing if this works

--- utils/actions.ts
+++ utils/actions.ts
@@ -5,7 +5,7 @@ import { Appointment as NewAppointment } from './types';
 import { Appointment } from '@prisma/client';
 import db from './db';
 import { redirect } from 'next/navigation';
-import { revalidatePath, revalidateTag } from 'next/cache';
+import { revalidatePath } from 'next/cache';
 
 export const createAppointmentAction = async (
   formData: NewAppointment
