f5a2cfd0ce3d65bc1f7403161ec6cf093322f8b5
E. Hannah <echannah631@gmail.com>
2025-02-21 01:15:03 -0500

    adding a blurb to review page

--- app/(public)/review/page.tsx
+++ app/(public)/review/page.tsx
@@ -1,8 +1,8 @@
-import Container from '@/components/global/Container';
-import SectionTitle from '@/components/global/SectionTitle';
-import ReviewForm from '@/components/review/ReviewForm';
-import { fetchServiceInfo } from '@/utils/actions';
-import React from 'react';
+import Container from "@/components/global/Container";
+import SectionTitle from "@/components/global/SectionTitle";
+import ReviewForm from "@/components/review/ReviewForm";
+import { fetchServiceInfo } from "@/utils/actions";
+import React from "react";
 
 const ReviewPage = async () => {
   const serviceNamesId = await fetchServiceInfo();
@@ -15,6 +15,19 @@ const ReviewPage = async () => {
         alignment='left'
       />
       <Container className='py-20'>
+        <p className='mb-8 text-4xl font-bold'>
+          I&#39;d Love to Hear From You!
+        </p>
+        <p className='mb-6 font-light text-xl'>
+          Your feedback is important to me! If you enjoyed my makeup services,
+          let me know by leaving a review. Your words not only help me improve
+          but also guide others in finding the perfect beauty experience.
+          Whether it&#39;s a soft glam or a bold transition, I&#39;d love to
+          hear about your experience.
+        </p>
+        <p className='mb-6 font-light text-xl'>
+          Thank you for your support 🫶🏿.
+        </p>
         <ReviewForm serviceNames={serviceNames} />
       </Container>
     </main>
