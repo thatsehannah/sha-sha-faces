bb234cb0f23289627c779bf9d08c2f88d1f87f77
E. Hannah <echannah631@gmail.com>
2025-05-09 09:13:27 -0400

    removing ability to click on desciption in service card

--- components/home/components/ServicesCard.tsx
+++ components/home/components/ServicesCard.tsx
@@ -45,12 +45,10 @@ const ServicesCard = ({ service }: ServiceCardProps) => {
             </div>
           </CardDescription>
         </CardHeader>
-        <CardContent className='font-light text-lg text-black overflow-hidden cursor-pointer'>
-          <Link href={`/services#${service.id}`}>
-            <div className='h-36 overflow-y-auto'>
-              <p className='line-clamp-5'>{service.description}</p>
-            </div>
-          </Link>
+        <CardContent className='font-light text-lg text-black overflow-hidden'>
+          <div className='h-36 overflow-y-auto'>
+            <p className='line-clamp-5'>{service.description}</p>
+          </div>
         </CardContent>
         <CardFooter className=''>
           <Button
