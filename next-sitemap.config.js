fd8fd4b16078dbaeb1c35935426caf99e064bbac
E. Hannah <echannah631@gmail.com>
2025-08-26 16:41:26 -0400

    creating sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: "https://www.shashafaces.com",
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: "weekly",
+  priority: 0.7,
+  exclude: ["/admin/*"],
+};
