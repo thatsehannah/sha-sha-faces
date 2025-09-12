827f91307af29f6d4f7d02e50112dc836a27d388
E. Hannah <echannah631@gmail.com>
2025-09-11 22:40:46 -0400

    making status cards clickable on admin page

--- components/dashboard/components/OverviewCard.tsx
+++ components/dashboard/components/OverviewCard.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 import React from "react";
 
 type OverviewCardProps = {
@@ -9,21 +10,23 @@ type OverviewCardProps = {
 
 const OverviewCard = ({ title, value, caption }: OverviewCardProps) => {
   return (
-    <Card
-      data-status={title}
-      className="w-42 lg:w-72 data-[status='Pending']:bg-orange-200 data-[status='Confirmed']:bg-blue-200 data-[status='Completed']:bg-green-200 data-[status='Canceled']:bg-red-200 text-black"
-    >
-      <CardHeader className='p-4 pb-1'>
-        <CardTitle className={`capitalize text-[1rem] lg:text-lg font-light`}>
-          <span className='uppercase font-bold tracking-wide'>{title}</span>{" "}
-          Appointments
-        </CardTitle>
-      </CardHeader>
-      <CardContent className='p-6 pt-0'>
-        <p className='lg:text-3xl text-2xl font-bold '>{value}</p>
-        {caption && <p>{caption}</p>}
-      </CardContent>
-    </Card>
+    <Link href={`/admin/appointments?status=${title.toLowerCase()}`}>
+      <Card
+        data-status={title}
+        className="w-42 lg:w-72 data-[status='Pending']:bg-orange-200 data-[status='Confirmed']:bg-blue-200 data-[status='Completed']:bg-green-200 data-[status='Canceled']:bg-red-200 text-black"
+      >
+        <CardHeader className='p-4 pb-1'>
+          <CardTitle className={`capitalize text-[1rem] lg:text-lg font-light`}>
+            <span className='uppercase font-bold tracking-wide'>{title}</span>{" "}
+            Appointments
+          </CardTitle>
+        </CardHeader>
+        <CardContent className='p-6 pt-0'>
+          <p className='lg:text-3xl text-2xl font-bold '>{value}</p>
+          {caption && <p>{caption}</p>}
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
 
